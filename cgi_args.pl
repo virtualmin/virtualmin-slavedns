c5f6ae3905d8ee8283394b7f5fce12d333377409
Jamie Cameron <jcameron@webmin.com>
2009-06-12 22:04:22 +0000

    Participate in Webmin search

---
+++ cgi_args.pl
@@ -0,0 +1,13 @@
+
+do 'virtualmin-slavedns-lib.pl';
+
+sub cgi_args
+{
+my ($cgi) = @_;
+my ($d) = grep { &virtual_server::can_edit_domain($_) &&
+	         $_->{$module_name} } &virtual_server::list_domains();
+if ($cgi eq 'edit.cgi') {
+	return $d ? 'dom='.&urlize($d->{'dom'}) : 'none';
+	}
+return undef;
+}
